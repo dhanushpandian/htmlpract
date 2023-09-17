e4684f5e14ab22e988230698bfd69e61fdc454c6
Dash <dhanushdpandian@gmail.com>
2023-09-17 21:22:30 +0530

    calc js

---
+++ exam.js
@@ -0,0 +1,13 @@
+console.log("hello world!");
+
+//let x = myFunction(4, 3);
+function myFunction(a, b) {
+  console.log(a+b);
+}
+
+for(let i=0;i<10;i++){
+    if (i%2==0){
+        console.log(i+1);
+    }
+}
+
